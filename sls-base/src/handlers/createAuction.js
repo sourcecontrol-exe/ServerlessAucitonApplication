5ef2e1a8632bfd637d1f5f32554c58f74bde53f9
Swetabh Subham <swetabh.subham@gmail.com>
2021-01-23 04:47:21 +0530

    Crud done

--- sls-base/src/handlers/createAuction.js
+++ sls-base/src/handlers/createAuction.js
@@ -14,7 +14,10 @@ async function createAuction(event, context) {
     id:uuid(),
     title,
     status:'OPEN',
-    createdAt: now.toISOString()
+    createdAt: now.toISOString(),
+    highestBid: {
+      amount:0
+    },
   };
   try{
     await dynamodb.put({
