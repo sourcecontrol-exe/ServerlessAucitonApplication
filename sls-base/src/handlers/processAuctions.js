1f6e4c7e5f9fbbd38b7743fcf8ef8b8b4b4e952c
Swetabh Subham <swetabh.subham@gmail.com>
2021-01-25 18:55:39 +0530

    new index formed

--- sls-base/src/handlers/processAuctions.js
+++ sls-base/src/handlers/processAuctions.js
@@ -1,5 +1,8 @@
+import {getEndedAucitons} from "../lib/getEndedAuctions";
+
 async function processAuctions(event,contex){
-    console.log("processing funciotns");
+    const auctionsToClose = await getEndedAucitons();
+    console.log(auctionsToClose);
 }
 
 export const handler = processAuctions;
